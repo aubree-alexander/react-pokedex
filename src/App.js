26f4431b83c328762966a8bd14bb734b69df9e52
Aubree Alexander <aubree.alexander@gmail.com>
2022-07-07 20:10:48 -0500

    state and fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { useState, useEffect } from 'react'
 import Header from './components/header'
+import SearchForm from './components/searchForm'
+import PokemonDetails from './components/pokemonDetails'
+import Spinner from './components/spinner'
 
 function App() {
+  //usestate
+  const [pokemon, setPokemon] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('ninetales')
+
+  //useeffect
+  //use empty array below to ensure this only runs once
+  useEffect(() => {
+    getPokemon()
+  }, [])
+
+  const getPokemon = () =>{
+    setLoading(true)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+      .then(res => res.json())
+      //save data into state
+      .then(json => setPokemon(json))
+      .catch(err => setError('Pokemon not found.'))
+      //no matter if .success or .catch runs, .finally will always run
+      .finally(() => setLoading(false))
+  }
+
   return (
+    <>
     <Header />
+    <SearchForm />
+    <PokemonDetails />
+    <Spinner />
+    </>
   );
 }
 
--- src/components/container.js
+++ src/components/container.js
@@ -1,3 +1,5 @@
+//this component will be reusable
+
 const Container = props => {
     return (
         //hardcoded container class; if user passes in props, it also has new prop classname, or output an empty string if no class is defined
